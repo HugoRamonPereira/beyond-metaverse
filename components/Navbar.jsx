5f3f6abb8e9413c8cb60adb705c15d04d06a53d5
Hugo Ramon <ramone.techie@gmail.com>
2023-05-11 21:12:38 -0300

    Hero section and its animations

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => (
       <h2
         className="font-extrabold text-[24px] leading-[30px] text-white"
       >
-        BEYOND METAVERSE
+        METAVERSUS
       </h2>
       <img src="/menu.svg" alt="menu" className="w-[24px] h-[24px] object-contain" />
     </div>
